7bdc0ae16c3fac2fe5fae6562181f4abe76dbfb8
saadsalmanakram <154062428+SaadxSalman@users.noreply.github.com>
2026-01-02 09:29:47 +0500

    Axios Interceptors (JWT)

---
+++ client/lib/axios.ts
@@ -0,0 +1,13 @@
+// client/lib/axios.ts
+import axios from "axios";
+import { getSession } from "next-auth/react";
+
+const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
+
+api.interceptors.request.use(async (config) => {
+  const session = await getSession();
+  if (session?.accessToken) {
+    config.headers.Authorization = `Bearer ${session.accessToken}`;
+  }
+  return config;
+});
