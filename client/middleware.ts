79092cea62a5c687de3e5eb84ef6262fcfdf1e85
saadsalmanakram <154062428+SaadxSalman@users.noreply.github.com>
2026-01-02 12:38:20 +0500

    Create middleware.ts

---
+++ client/middleware.ts
@@ -0,0 +1,10 @@
+// client/middleware.ts
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/auth/login",
+  },
+});
+
+export const config = { matcher: ["/workspaces/:path*", "/profile"] };
